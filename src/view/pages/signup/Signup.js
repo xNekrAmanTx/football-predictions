8cfd864c1523a455fce67ca16ffb7b779415be31
GarnikDavtyan <davtyangarnik95@gmail.com>
2020-01-28 21:26:29 +0400

    removed cssBaseline from Signup

--- src/view/pages/signup/Signup.js
+++ src/view/pages/signup/Signup.js
@@ -53,7 +53,6 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
@@ -90,7 +89,6 @@ export default function SignUp() {
 
     return (
         <Container component="main" maxWidth="xs">
-            <CssBaseline />
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
                     <LockOutlinedIcon />
